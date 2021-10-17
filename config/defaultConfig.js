0103af3077ad729c5d4f1bc48d1851ba9fcf29a5
wangxiui <1036900394@qq.com>
2021-10-17 11:42:08 +0800

    refactor: use ES6 Syntax;

--- config/defaultConfig.js
+++ config/defaultConfig.js
@@ -1,11 +1,11 @@
-module.exports = {
+export default {
   fontWeight: {
     thin: 100,
     extralight: 200,
     light: 300,
     normal: 400,
     medium: 500,
-    semibold: 600,
+    semibold: '#fff1f0',
     bold: 700,
     extrabold: 800,
     black: 900
