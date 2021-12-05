82c449f64b2cce1aec9faba3e080506ec31c356f
wangxiui <1036900394@qq.com>
2021-12-05 19:03:11 +0800

    refactor: extract escapeSelector to escapeClassName.js

--- src/util/defineClass.js
+++ src/util/defineClass.js
@@ -1,17 +1,9 @@
 import postcss from 'postcss'
 import _ from 'lodash'
+import escapeClassName from './escapeClassName'
 
-/**
- * 转译斜杠
- * 例：（1/10）
- * @param selector
- * @returns {*}
- */
-function escapeSelector (selector) {
-  return selector.replace(/([^A-Za-z0-9\\-])/g, '\\$1')
-}
 export default function (className, properties) {
-  const kebabClass = escapeSelector(className)
+  const kebabClass = escapeClassName(className)
   const decls = _.map(properties, (value, prop) => {
     return postcss.decl({
       prop: _.kebabCase(prop),
